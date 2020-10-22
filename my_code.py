1e69bed0ef2b1f3ecdee9d9fe1c13d05fd6bd061
yesak1 <23kkazliner@athenian.org>
2020-10-22 07:37:49 +0000

    30 Min of work for homework.

--- my_code.py
+++ my_code.py
@@ -2,5 +2,21 @@
 #  
 # A note on style: Dictionaries can be defined before or after functions.
 
+gameboard = {'TL': ' ', 'ML': ' ', 'BL': ' ', 'TM': ' ', 'MM': ' ', 'BM': ' ', 'TR': ' ', 'MR': ' ', 'BR': ' '}
+
+def ttt():
+    print (" | | ")
+    print ('-+-+-')
+    print (" | | ")
+    print ('-+-+-')
+    print (" | | ")
+    while True:
+        print("player 1 is X. Player 2 is O")
+        player1=input('Player 1, choose your spot to place X (TL, ML, BL, TM, MM, BM, TR, MR, BR ) ')
+        
+        
+
+ttt()
+
 
 
