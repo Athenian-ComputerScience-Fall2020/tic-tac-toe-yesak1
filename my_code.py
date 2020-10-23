5e86accd808936d736e126df64c96d81dc54b1a2
yesak1 <23kkazliner@athenian.org>
2020-10-23 19:15:12 +0000

    Megan check

--- my_code.py
+++ my_code.py
@@ -1,22 +1,32 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+#  Megan 
 # A note on style: Dictionaries can be defined before or after functions.
 
-gameboard = {'TL': ' ', 'ML': ' ', 'BL': ' ', 'TM': ' ', 'MM': ' ', 'BM': ' ', 'TR': ' ', 'MR': ' ', 'BR': ' '}
+gb = {'TL': ' ', 'ML': ' ', 'BL': ' ', 'TM': ' ', 'MM': ' ', 'BM': ' ', 'TR': ' ', 'MR': ' ', 'BR': ' '}
 
-def ttt():
-    print (" | | ")
+def ttt(gb):
+    print ((gb["TL"]) + '|' + (gb["TM"]) + '|' + (gb["TR"]))
     print ('-+-+-')
-    print (" | | ")
+    print ((gb["ML"]) + '|' + (gb["MM"]) + '|' + (gb["MR"]))
     print ('-+-+-')
-    print (" | | ")
-    while True:
-        print("player 1 is X. Player 2 is O")
-        player1=input('Player 1, choose your spot to place X (TL, ML, BL, TM, MM, BM, TR, MR, BR ) ')
+    print ((gb['BL']) + '|' + (gb["BM"]) + '|' + (gb["BR"]))
+    player1()
+    
+    
+def player1(): 
+    print("player 1 is X. Player 2 is O")
+    player1=input('Player 1, choose your spot to place X (TL, ML, BL, TM, MM, BM, TR, MR, BR ) ')
+    gb[player1]=="X"
+    ttt(gb)    
+    
+
+    
+
+    
         
         
 
-ttt()
+ttt(gb)
 
 
 
